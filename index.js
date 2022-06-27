c20d5b571933f11342c2dd49299ff328c9d43cb1
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-06-28 01:04:05 +0530

    Added concurrently package and modified port number

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 app.use('/follow', followerRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
